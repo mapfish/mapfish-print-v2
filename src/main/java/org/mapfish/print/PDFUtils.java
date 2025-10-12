f0c8f44c94cc23491538d113f208a60505358ddc
Jody Garnett <jody.garnett@gmail.com>
2025-10-11 17:44:12 -0700

    sync versions for GeoTools 34.0 bill-of-materials, and geoserver 2.28.0 release

--- src/main/java/org/mapfish/print/PDFUtils.java
+++ src/main/java/org/mapfish/print/PDFUtils.java
@@ -40,12 +40,12 @@ import com.lowagie.text.pdf.PdfPCell;
 import com.lowagie.text.pdf.PdfPTable;
 import com.lowagie.text.pdf.PdfTemplate;
 
+import org.apache.batik.anim.dom.SAXSVGDocumentFactory;
 import org.apache.batik.bridge.BridgeContext;
 import org.apache.batik.bridge.DocumentLoader;
 import org.apache.batik.bridge.GVTBuilder;
 import org.apache.batik.bridge.UserAgent;
 import org.apache.batik.bridge.UserAgentAdapter;
-import org.apache.batik.dom.svg.SAXSVGDocumentFactory;
 import org.apache.batik.dom.svg.SVGDocumentFactory;
 import java.io.ByteArrayOutputStream;
 import org.apache.batik.gvt.GraphicsNode;
