24bb48812f338bf6704976f335d5d4a59d05e4b3
josegar74 <josegar74@gmail.com>
2026-08-04 16:37:19 +0200

    Upgrade pdfbox to version 3.0.8

--- src/main/java/org/mapfish/print/output/InMemoryJaiMosaicOutputFactory.java
+++ src/main/java/org/mapfish/print/output/InMemoryJaiMosaicOutputFactory.java
@@ -22,6 +22,7 @@ package org.mapfish.print.output;
 import com.lowagie.text.DocumentException;
 import org.apache.logging.log4j.Logger;
 import org.apache.logging.log4j.LogManager;
+import org.apache.pdfbox.Loader;
 import org.apache.pdfbox.pdmodel.PDDocument;
 import org.apache.pdfbox.pdmodel.PDPage;
 import org.apache.pdfbox.rendering.ImageType;
@@ -153,8 +154,7 @@ public class InMemoryJaiMosaicOutputFactory implements OutputFormatFactory {
 
         private List<BufferedImage> createImages(PJsonObject jsonSpec, File tmpFile, RenderingContext context) throws IOException {
             List<BufferedImage> images = new ArrayList<BufferedImage>();
-            PDDocument pdf = PDDocument.load(tmpFile);
-            try {
+            try (PDDocument pdf = Loader.loadPDF(tmpFile)) {
                 @SuppressWarnings("unchecked")
                 PDFRenderer pdfRenderer = new PDFRenderer(pdf);
                 for (PDPage page : pdf.getPages())
@@ -162,8 +162,6 @@ public class InMemoryJaiMosaicOutputFactory implements OutputFormatFactory {
                     BufferedImage img = pdfRenderer.renderImageWithDPI(images.size(), calculateDPI(context, jsonSpec), ImageType.ARGB);
                     images.add(img);
                 }
-            } finally {
-                pdf.close();
             }
             return images;
         }
